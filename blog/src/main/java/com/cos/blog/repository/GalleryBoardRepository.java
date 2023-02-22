c89f712f589c77c0a6ccfee15ac0540a75c37a0a
LEE JEONGHA <jeongha365@gmail.com>
2023-02-22 09:59:05 +0900

    feat : [이정하] 갤러리 게시판 조회수 기능 구현[bowling-5]

--- blog/src/main/java/com/cos/blog/repository/GalleryBoardRepository.java
+++ blog/src/main/java/com/cos/blog/repository/GalleryBoardRepository.java
@@ -1,9 +1,16 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.cos.blog.model.GalleryBoard;
 
 public interface GalleryBoardRepository extends JpaRepository<GalleryBoard, Integer>{
+	
+	@Modifying
+	@Query("update GalleryBoard g set g.galleryBoardCount = g.galleryBoardCount + 1 where g.id = :id")
+	int CountUp(int id);
 	
 }
 
